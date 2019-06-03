bc3e8a2c34e89122798a4d7674a32af37e299c9f
utkarshsingh99 <utkarshsingh369@gmail.com>
2019-06-04 02:55:27 +0530

    Modified routes

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -7,7 +7,7 @@ import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from "./types";
 // Register User
 export const registerUser = (userData, history) => dispatch => {
   axios
-    .post("/api/users/register", userData)
+    .post("https://cupido-backend.herokuapp.com/api/users/register", userData)
     .then(res => history.push("/login"))
     .catch(err =>
       dispatch({
@@ -20,7 +20,7 @@ export const registerUser = (userData, history) => dispatch => {
 // Login - get user token
 export const loginUser = userData => dispatch => {
   axios
-    .post("/api/users/login", userData)
+    .post("https://cupido-backend.herokuapp.com/api/users/login", userData)
     .then(res => {
       // Save to localStorage
 
